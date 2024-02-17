7cc6f3e0c1e9cd7297f0b3a220dd231cef0722f1
Cédric Theveneau <87996087+CedricTheveneau@users.noreply.github.com>
2024-02-17 12:38:36 +0100

    Changed the branding and switched up the colors; added hero section support

--- LightningUI/blogData.js
+++ LightningUI/blogData.js
@@ -6,17 +6,17 @@ const blogData = [
     tags: "<span class='tag feature'>Actualité agricole</span>",
     date: "<p>-&nbsp;&nbsp; 11 Février 2024</p>",
     content:
-      "<p>Nous savons à quel point l'agriculture est avant tout une vocation. C'est n'est pas qu'un métier, c'est une passion, qui se cultive au quotidien. Des tâches aussi prenantes que difficiles à accomplir au quotidien.</p><p>Nous espérons de tout coeur, avec notre équipe, que vous obtiendrez gain de cuase, et nous nous battrons à vos côtés pour obtenir de meilleures conditions de travail pour nous tous.</p><br/><p>L'équipe KGB</p>",
-    img: "../LightningUI/assets/logoLight.svg",
+      "<p>Nous savons à quel point l'agriculture est avant tout une vocation. C'est n'est pas qu'un métier, c'est une passion, qui se cultive au quotidien. Des tâches aussi prenantes que difficiles à accomplir au quotidien.</p><p>Nous espérons de tout coeur, avec notre équipe, que vous obtiendrez gain de cause, et nous nous battrons à vos côtés pour obtenir de meilleures conditions de travail pour nous tous.</p><br/><p>L'équipe AgriServices</p>",
+    img: "../assets/logoColor.svg",
   },
   {
-    title: "Lancement de KGB",
+    title: "Lancement de AgriServices",
     summary:
       "<p>Pour ce premier point d'actualité, nous allons revenir sur quelques points clés de la création de l'entreprise, ainsi que les services qu'elle met à disposition des agriculteurs.</p>",
     tags: "<span class='tag release'>Lancement</span>",
     date: "<p>-&nbsp;&nbsp; 11 Février 2024</p>",
     content:
-      "<p>Tout d'abord, nous tenons à vous remercier pour l'intérêt que vous porter à notre projet. C'est avant tout grâce à vous, qui lisez ce post que notre entreprise pourra fleurir et vous proposer des services de qualité toujours supérieure.</p><p>KGB a vu le jour suite à mon départ de l'entreprise pour laquelle je travaillais avant. J'ai décidé, avec un ami, de fonder KGB, afin de permettre aux agriculteurs, de tous types d'exploitations, de bénéficier de services de qualité, sans avoir à se procurer l'équipement nécessaire à ces travaux ponctuels.</p><p>C'est pourquoi nous avons décidé de nous équiper d'une machine et de venir vous aider sur votre exploitation en nous occupant de ces travaux à votre place. Le but étant de vous faire gagner du temps et de l'argent, et de vous permettre de pouvoir rester focalisé sur ce qui vous importe le plus et vous tient le plus à coeur : votre exploitation.</p><p>Merci encore pour votre temps, nous espérons vous rencontrer bientôt, sur votre exploitation.</p><br/><p>L'équipe KGB</p>",
-    img: "../LightningUI/assets/logoLight.svg",
+      "<p>Tout d'abord, nous tenons à vous remercier pour l'intérêt que vous porter à notre projet. C'est avant tout grâce à vous, qui lisez ce post que notre entreprise pourra fleurir et vous proposer des services de qualité toujours supérieure.</p><p>AgriServices a vu le jour suite à mon départ de l'entreprise pour laquelle je travaillais avant. J'ai décidé, avec un ami, de fonder AgriServices, afin de permettre aux agriculteurs, de tous types d'exploitations, de bénéficier de services de qualité, sans avoir à se procurer l'équipement nécessaire à ces travaux ponctuels.</p><p>C'est pourquoi nous avons décidé de nous équiper d'une machine et de venir vous aider sur votre exploitation en nous occupant de ces travaux à votre place. Le but étant de vous faire gagner du temps et de l'argent, et de vous permettre de pouvoir rester focalisé sur ce qui vous importe le plus et vous tient le plus à coeur : votre exploitation.</p><p>Merci encore pour votre temps, nous espérons vous rencontrer bientôt, sur votre exploitation.</p><br/><p>L'équipe AgriServices</p>",
+    img: "../assets/logoNameColor.svg",
   },
 ];
